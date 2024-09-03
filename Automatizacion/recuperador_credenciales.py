028a5df3f90a5544c48842df326209d8f04c678a
SPIDERMIKE <miguel.olguin@comtecom.com.mx>
2024-09-03 10:06:40 -0600

    new map close pop up

--- Automatizacion/recuperador_credenciales.py
+++ Automatizacion/recuperador_credenciales.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 USER = ''
 PASSWORD = ''
 URL = "https://mobilityado.awsapps.com/start/#/?tab=accounts";
-ELEMENTS = {'ELEMENT0':{'ELEMENT':'//strong[contains(.,"AWS_ACCOUNT")]','SELECTOR':'XPATH','ACCTION':1}, 'ELEMENT1':{'ELEMENT':'//a[contains(.,"Access keys")]','SELECTOR':'XPATH','ACCTION':1}, 'ELEMENT2':{'ELEMENT':'//div[4]/div/div[2]/div/div[2]/div/div[2]/div/div/input','SELECTOR':'XPATH','ACCTION':3,'VALUE':'ACCESS_KEY_ID'},'ELEMENT3':{'ELEMENT':'//div[4]/div/div[2]/div/div[3]/div/div[2]/div/div/input','SELECTOR':'XPATH','ACCTION':3,'VALUE':'SECRET_ACCESS_KEY'},'ELEMENT4':{'ELEMENT':'//div[4]/div/div[2]/div/div[4]/div/div[2]/div/div/input','SELECTOR':'XPATH','ACCTION':3,'VALUE':'SESSION_TOCKEN'},'ELEMENT5':{'ELEMENT':'awsui_dismiss-control_1d2i7_84i8r_379','SELECTOR':'CLASS','ACCTION':1},'ELEMENT6':{'ELEMENT':'//strong[contains(.,"AWS_ACCOUNT")]','SELECTOR':'XPATH','ACCTION':1}}
+ELEMENTS = {'ELEMENT0':{'ELEMENT':'//strong[contains(.,"AWS_ACCOUNT")]','SELECTOR':'XPATH','ACCTION':1}, 'ELEMENT1':{'ELEMENT':'//a[contains(.,"Access keys")]','SELECTOR':'XPATH','ACCTION':1}, 'ELEMENT2':{'ELEMENT':'//div[4]/div/div[2]/div/div[2]/div/div[2]/div/div/input','SELECTOR':'XPATH','ACCTION':3,'VALUE':'ACCESS_KEY_ID'},'ELEMENT3':{'ELEMENT':'//div[4]/div/div[2]/div/div[3]/div/div[2]/div/div/input','SELECTOR':'XPATH','ACCTION':3,'VALUE':'SECRET_ACCESS_KEY'},'ELEMENT4':{'ELEMENT':'//div[4]/div/div[2]/div/div[4]/div/div[2]/div/div/input','SELECTOR':'XPATH','ACCTION':3,'VALUE':'SESSION_TOCKEN'},'ELEMENT5':{'ELEMENT':'awsui_dismiss-control_1d2i7_wb8ll_431','SELECTOR':'CLASS','ACCTION':1},'ELEMENT6':{'ELEMENT':'//strong[contains(.,"AWS_ACCOUNT")]','SELECTOR':'XPATH','ACCTION':1}}
 ACCOUNTS = [ {'ACCOUNT':'AWS-Servicios-Test','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Data-Dev','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Data-Prod','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Data-Test','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'fgs-dev','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'fgs-Prod','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'fgs-test','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False},{'ACCOUNT':'GTFS-Dev','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False},{'ACCOUNT':'GTFS-Prod','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False},{'ACCOUNT':'GTFS-Test','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Mone-Dev','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Mone-Prod','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Mone-test','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'MoneRepo-Dev','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'MoneRepo-Prod','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'MoneRepo-Test','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Servicios-Dev','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False}, {'ACCOUNT':'Servicios-Prod','ACCESS_KEY_ID':'','SECRET_ACCESS_KEY':'','SESSION_TOCKEN':'','GET':False} ]
 IS_PRINT = False
-sandbox_enabled = True
+sandbox_enabled = False
 
 def print_task(msg):
     if IS_PRINT:
@@ -64,7 +64,7 @@ def getCredencialesAWS(user_input,pass_input,url_input,code_input):
            for key, value in ELEMENTS.items():
                element_rp = value['ELEMENT'].replace("AWS_ACCOUNT",account['ACCOUNT'])
                elemento = findElement(driver,element_rp,value['SELECTOR'])
-               print(f"OBTENIENDO CREDENCIALES DE CUANTA: {account['ACCOUNT']}")
+               print(f"OBTENIENDO CREDENCIALES DE CUENTA: {account['ACCOUNT']}")
                if value['ACCTION'] == 1:
                    #driver.execute_script("arguments[0].scrollIntoView(true);", elemento)   
                    #elemento.click()
